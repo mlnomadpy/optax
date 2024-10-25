26f528c3128513074f75fe5678ee5ab3ef7b2f5c
Taha Bouhsine <45195299+mlnomadpy@users.noreply.github.com>
2024-10-25 04:35:23 +0000

    ss

--- optax/contrib/_cocob.py
+++ optax/contrib/_cocob.py
@@ -39,80 +39,78 @@ class COCOBState(NamedTuple):
   reward: base.Updates
 
 
-def scale_by_cocob(
-    alpha: float = 100, eps: float = 1e-8
-) -> base.GradientTransformation:
-  """Rescale updates according to the COntinuous COin Betting algorithm.
+def scale_by_cocob(alpha: float = 100, eps: float = 1e-8) -> base.GradientTransformation:
+    """Rescale updates according to the COntinuous COin Betting algorithm.
 
-  Algorithm for stochastic subgradient descent. Uses a gambling algorithm to
-  find the minimizer of a non-smooth objective function by accessing its
-  subgradients. All we need is a good gambling strategy. See Algorithm 2 of:
+    Algorithm for stochastic subgradient descent. Uses a gambling algorithm to
+    find the minimizer of a non-smooth objective function by accessing its
+    subgradients. All we need is a good gambling strategy. See Algorithm 2 of:
 
-  References:
-    [Orabona & Tommasi, 2017](https://arxiv.org/pdf/1705.07795.pdf)
+    References:
+      [Orabona & Tommasi, 2017](https://arxiv.org/pdf/1705.07795.pdf)
 
-  Args:
-    alpha: fraction to bet parameter of the COCOB optimizer
-    eps: jitter term to avoid dividing by 0
+    Args:
+      alpha: fraction to bet parameter of the COCOB optimizer
+      eps: jitter term to avoid dividing by 0
 
-  Returns:
-    A `GradientTransformation` object.
-  """
+    Returns:
+      A `GradientTransformation` object.
+    """
 
-  def init_fn(params):
-    init_adapt = otu.tree_zeros_like(params)
-    init_scale = otu.tree_ones_like(params)
-    init_scale = otu.tree_scalar_mul(eps, init_scale)
-    return COCOBState(
-        init_particles=params,
-        cumulative_gradients=init_adapt,
-        scale=init_scale,
-        subgradients=init_adapt,
-        reward=init_adapt,
-    )
+    def init_fn(params):
+      init_adapt = otu.tree_zeros_like(params)
+      init_scale = otu.tree_ones_like(params)
+      init_scale = otu.tree_scalar_mul(eps, init_scale)
+      return COCOBState(
+          init_particles=params,
+          cumulative_gradients=init_adapt,
+          scale=init_scale,
+          subgradients=init_adapt,
+          reward=init_adapt,
+      )
 
-  def update_fn(updates, state, params):
-    init_particles, cumulative_grads, scale, subgradients, reward = state
+    def update_fn(updates, state, params):
+      init_particles, cumulative_grads, scale, subgradients, reward = state
 
-    scale = jax.tree.map(
-        lambda L, c: jnp.maximum(L, jnp.abs(c)), scale, updates
-    )
-    subgradients = jax.tree.map(
-        lambda G, c: G + jnp.abs(c), subgradients, updates
-    )
-    reward = jax.tree.map(
-        lambda R, c, p, p0: jnp.maximum(R - c * (p - p0), 0),
-        reward,
-        updates,
-        params,
-        init_particles,
-    )
-    cumulative_grads = jax.tree.map(
-        lambda C, c: C - c, cumulative_grads, updates
-    )
+      scale = jax.tree.map(
+          lambda L, c: jnp.maximum(L, jnp.abs(c)), scale, updates
+      )
+      subgradients = jax.tree.map(
+          lambda G, c: G + jnp.abs(c), subgradients, updates
+      )
+      reward = jax.tree.map(
+          lambda R, c, p, p0: jnp.maximum(R - c * (p - p0), 0),
+          reward,
+          updates,
+          params,
+          init_particles,
+      )
+      cumulative_grads = jax.tree.map(
+          lambda C, c: C - c, cumulative_grads, updates
+      )
 
-    new_updates = jax.tree.map(
-        lambda p, p0, C, L, G, R: (
-            -p + (p0 + C / (L * jnp.maximum(G + L, alpha * L)) * (L + R))
-        ),
-        params,
-        init_particles,
-        cumulative_grads,
-        scale,
-        subgradients,
-        reward,
-    )
+      new_updates = jax.tree.map(
+          lambda p, p0, C, L, G, R: (
+              -p + (p0 + C / (L * jnp.maximum(G + L, alpha * L)) * (L + R))
+          ),
+          params,
+          init_particles,
+          cumulative_grads,
+          scale,
+          subgradients,
+          reward,
+      )
 
-    new_state = COCOBState(
-        init_particles=init_particles,
-        cumulative_gradients=cumulative_grads,
-        scale=scale,
-        subgradients=subgradients,
-        reward=reward,
-    )
-    return new_updates, new_state
+      new_state = COCOBState(
+          init_particles=init_particles,
+          cumulative_gradients=cumulative_grads,
+          scale=scale,
+          subgradients=subgradients,
+          reward=reward,
+      )
+      return new_updates, new_state
 
-  return base.GradientTransformation(init_fn, update_fn)
+    return base.GradientTransformation(init_fn, update_fn)
 
 
 def cocob(
@@ -122,27 +120,29 @@ def cocob(
     weight_decay: float = 0,
     mask: Optional[Union[Any, Callable[[base.Params], Any]]] = None,
 ) -> base.GradientTransformation:
-  """Rescale updates according to the COntinuous COin Betting algorithm.
+    """Rescale updates according to the COntinuous COin Betting algorithm.
 
-  Algorithm for stochastic subgradient descent. Uses a gambling algorithm to
-  find the minimizer of a non-smooth objective function by accessing its
-  subgradients. All we need is a good gambling strategy. See Algorithm 2 of:
+    Algorithm for stochastic subgradient descent. Uses a gambling algorithm to
+    find the minimizer of a non-smooth objective function by accessing its
+    subgradients. All we need is a good gambling strategy. See Algorithm 2 of:
 
-  References:
-    [Orabona & Tommasi, 2017](https://arxiv.org/pdf/1705.07795.pdf)
+    References:
+      [Orabona & Tommasi, 2017](https://arxiv.org/pdf/1705.07795.pdf)
 
-  Args:
-    learning_rate: optional learning rate to e.g. inject some scheduler
-    alpha: fraction to bet parameter of the COCOB optimizer
-    eps: jitter term to avoid dividing by 0
-    weight_decay: L2 penalty
-    mask: mask for weight decay
+    Args:
+      learning_rate: optional learning rate to e.g. inject some scheduler
+      alpha: fraction to bet parameter of the COCOB optimizer
+      eps: jitter term to avoid dividing by 0
+      weight_decay: L2 penalty
+      mask: mask for weight decay
 
-  Returns:
-    A `GradientTransformation` object.
-  """
-  return combine.chain(
-      transform.add_decayed_weights(weight_decay, mask),
-      transform.scale_by_learning_rate(learning_rate, flip_sign=False),
-      scale_by_cocob(alpha, eps),
-  )
+    Returns:
+      A `GradientTransformation` object.
+    """
+    return combine.chain(
+        transform.add_decayed_weights(weight_decay, mask),
+        transform.scale_by_learning_rate(learning_rate, flip_sign=False),
+        scale_by_cocob(alpha, eps),
+    )
+
+__all__ = ['COCOBState', 'scale_by_cocob', 'cocob']
